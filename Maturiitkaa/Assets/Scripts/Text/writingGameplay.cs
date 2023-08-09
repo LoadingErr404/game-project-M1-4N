273dea5a9dae0d87a42ba0efbcedf92a02ba52c2
LoadingErr404 <bartakova.ma@gmail.com>
2023-08-10 00:40:48 +0200

    funkce pro vypisování stisku
    funkce buffWord() zaznamená jakýkoliv stisk klávesnice a předá jej jako znak do stringu myWord
    pokud jde o backspace, vymaže poslední znak
    -> musí se dovymyslet že bude mazat i při držení -> nejspíš bude vedeno jinou funkcí

--- Maturiitkaa/Assets/Scripts/Text/writingGameplay.cs
+++ Maturiitkaa/Assets/Scripts/Text/writingGameplay.cs
@@ -6,24 +6,47 @@ using TMPro;
 public class writingGameplay : MonoBehaviour
 {
     public TMP_Text myText;
+    private string myWord;
 
     // Start is called before the first frame update
     void Start()
     {
         ClearText();
+        myWord="";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.anyKeyDown){ //ošetři jenom pro písmena, čísla a základní znaky ang klávesnice
+            foreach (char letter in Input.inputString){ //every possible key input
+                Debug.Log(letter);
+                buffWord(letter);
+                Debug.Log(myWord);
+                printWord();
+            }
+        }
     }
 
     private void ClearText(){ //clears output text
         myText.text="";
     }
 
-    private char BufferLetter(){
-        char letter = Input.GetKey(KeyCode.Return);
+    private void buffWord(char letter){ //buffers pressed characters
+        if(letter != '\b'){
+            myWord+=letter;
+        }
+        else{
+            myWord = myWord.Remove(myWord.Length - 1);
+        }
     }
+
+    private void printWord(){
+        myText.text=myWord;
+    }
+
+    
 }
+
+
+    
