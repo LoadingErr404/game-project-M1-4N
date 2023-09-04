3a8ebcb63b923f1c06ee74246438d64f0ff94d88
LoadingErr404 <bartakova.ma@gmail.com>
2023-09-04 22:42:11 +0200

    Nefunguje movement nějak
    musím spravit idk proč to nejede
    i live in Spain but the S is silent

--- Maturiitkaa/Assets/Scripts/CharacterController2D.cs
+++ Maturiitkaa/Assets/Scripts/CharacterController2D.cs
@@ -1,20 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody2D;
+    
+    [Header("Movement physics")]
+    public float movementForce;
+
+    private Vector3 moveDir; //vector that will be handeling physics
+
+    private void Awake()
     {
-        
+        throw new NotImplementedException();
+        _rigidbody2D = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
+        float moveX = 0f;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-1, 0);
+            //transform.position += new Vector3(-1, 0);
+            moveX -= movementForce;
+
         }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.position += new Vector3(1, 0);
+            moveX += movementForce;
+        }
+
+        moveDir = new Vector3(moveX, 0);
+    }
+
+    private void FixedUpdate() //for physics
+    {
+        throw new NotImplementedException();
+        _rigidbody2D.velocity = moveDir;
     }
 }
