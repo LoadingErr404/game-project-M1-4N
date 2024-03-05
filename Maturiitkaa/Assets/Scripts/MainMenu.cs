6208297b2ab1d525f6b57ab2a9f6bdeee0db01f0
LoadingErr404 <bartakova.ma@gmail.com>
2024-03-06 00:16:25 +0100

    prvni setup main menu

---
+++ Maturiitkaa/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject optionMenu;
+    private void Start()
+    {
+        optionMenu.SetActive(false);
+    }
+
+    public void PlayGame() //loads next scene
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
